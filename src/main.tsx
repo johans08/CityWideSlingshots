a7f284179a252a46e68b268ed2c6f67a06075776
sara <retanasara003@gmail.com>
2025-03-03 12:18:28 -0600

    home page

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { HomePage } from './Pages/HomePage.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* HOLA */}
-    <App />
+    <HomePage/>
   </StrictMode>,
 )
